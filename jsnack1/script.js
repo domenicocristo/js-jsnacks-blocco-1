0f38df0b5d6968220962957dabd4cafe45f4c81e
MyUsername <my@email.com>
2021-10-28 14:38:15 +0200

    primo fine

---
+++ jsnack1/script.js
@@ -0,0 +1,8 @@
+let firstNumber = prompt("Digita un numero");
+let lastNumber = prompt("Digita un numero");
+
+if (firstNumber < lastNumber) {
+    console.log(lastNumber);
+} else {
+    console.log(firstNumber)
+}
